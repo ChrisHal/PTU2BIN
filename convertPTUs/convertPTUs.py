6f76cb677bc189e6a95d319dff32a397cf403eb5
Christian Halaszovich <chalaszovich@gmail.com>
2019-09-11 15:47:12 +0200

    Sdded small python script to batch convert PTU files.

---
+++ convertPTUs/convertPTUs.py
@@ -0,0 +1,23 @@
+
+#####################
+#####################
+### (C) 2019 C. R. Halaszovich
+### Tool to apply PTU2BIN to all PTU-files in current directory
+
+import glob
+import subprocess
+
+defaultchannel = 2
+toolcmd = "PTU2BIN"
+
+filelist=glob.glob('*.ptu')
+print("Found %i PTU files to convert."%len(filelist))
+for fn in filelist:
+	target = fn[:-4]+".bin"
+	print("\n###############\nConverting %s to %s\n"%(fn,target))
+	res=subprocess.run([toolcmd,fn,target,str(defaultchannel)])
+	if res.returncode != 0:
+		print("An error occured while converting file '%s'."%fn)
+
+
+
