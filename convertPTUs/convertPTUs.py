5fb9ada580fe3e48ac51c4c52f896dc687913910
Christian Halaszovich <chalaszovich@gmail.com>
2019-09-11 17:11:59 +0200

    Now we are converting whole directory trees, skipping already converted files.

--- convertPTUs/convertPTUs.py
+++ convertPTUs/convertPTUs.py
@@ -2,21 +2,26 @@
 #####################
 #####################
 ### (C) 2019 C. R. Halaszovich
-### Tool to apply PTU2BIN to all PTU-files in current directory
+### Tool to apply PTU2BIN to all PTU-files in current directory,
+### including subdirectories
 
+import os.path
 import glob
 import subprocess
 
 defaultchannel = 2
 toolcmd = "PTU2BIN"
 
-filelist=glob.glob('*.ptu')
+filelist=glob.glob('**/*.ptu', recursive=True)
 print("Found %i PTU files to convert."%len(filelist))
 for fn in filelist:
-	target = fn[:-4]+".bin"
-	print("\n###############\nConverting %s to %s\n"%(fn,target))
-	res=subprocess.run([toolcmd,fn,target,str(defaultchannel)])
-	if res.returncode != 0:
-		print("An error occured while converting file '%s'."%fn)
+    target = fn[:-4]+".bin"
+    if os.path.exists(target):
+        print("File '%s' exists. Skipping conversion for this file."%target)
+        continue
+    print("\n###############\nConverting %s to %s\n"%(fn,target))
+    res=subprocess.run([toolcmd,fn,target,str(defaultchannel)])
+    if res.returncode != 0:
+        print("An error occured while converting file '%s'."%fn)
 print("Press RETURN")
 input()
