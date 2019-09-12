0390b82611f0a6ac00c52eb06954f7b46aa62c24
Christian Halaszovich <chalaszovich@gmail.com>
2019-09-12 11:37:19 +0200

    some minor changes to the output of the python script

--- convertPTUs/convertPTUs.py
+++ convertPTUs/convertPTUs.py
@@ -17,18 +17,21 @@ print("Found %i PTU files to convert."%len(filelist))
 for fn in filelist:
     target = fn[:-4]+".bin"
     if os.path.exists(target):
-        print("File '%s' exists. Skipping conversion for this file."%target)
+        print("\nFile '%s' exists. Skipping conversion for this file."%target)
         continue
-    print("\n###############\nConverting %s to %s\n"%(fn,target))
+    print("\n###############\nconverting %s to %s"%(fn,target))
     res=subprocess.run([toolcmd,fn,target,str(defaultchannel)],capture_output=True,encoding="utf-8")
     if res.returncode != 0:
         print("An error occured while converting file '%s'."%fn)
         if(len(res.stderr)>0):
            print("ERROR: %s"%res.stderr)
     else:
-        textfilename=fn[:-4]+".txt"
-        textfile=open(textfilename,"w")
-        textfile.write(res.stdout)
-        textfile.close()
+        print("conversion finished")
+        if(len(res.stdout)>0):
+            textfilename=fn[:-4]+".txt"
+            textfile=open(textfilename,"w")
+            textfile.write(res.stdout)
+            textfile.close()
+            print("output has been written to txt-file")
 print("Press RETURN")
 input()
