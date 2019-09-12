bed78e19e59368a579fcda66ccbfe69612edda71
Christian Halaszovich <chalaszovich@gmail.com>
2019-09-12 11:28:28 +0200

    convertPTUs.py now saves the output of PTU2BIN ti a textfile for each converted PTU file.

--- convertPTUs/convertPTUs.py
+++ convertPTUs/convertPTUs.py
@@ -20,8 +20,15 @@ for fn in filelist:
         print("File '%s' exists. Skipping conversion for this file."%target)
         continue
     print("\n###############\nConverting %s to %s\n"%(fn,target))
-    res=subprocess.run([toolcmd,fn,target,str(defaultchannel)])
+    res=subprocess.run([toolcmd,fn,target,str(defaultchannel)],capture_output=True,encoding="utf-8")
     if res.returncode != 0:
         print("An error occured while converting file '%s'."%fn)
+        if(len(res.stderr)>0):
+           print("ERROR: %s"%res.stderr)
+    else:
+        textfilename=fn[:-4]+".txt"
+        textfile=open(textfilename,"w")
+        textfile.write(res.stdout)
+        textfile.close()
 print("Press RETURN")
 input()
