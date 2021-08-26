e0a18abc457c4ee123baa89e426d08a4c4ea7437
Christian Halaszovich <chalaszovich@gmail.com>
2021-08-26 17:13:36 +0200

    make RecordBuffer safer to use

--- PTU2BIN/RecordBuffer.h
+++ PTU2BIN/RecordBuffer.h
@@ -3,7 +3,7 @@
 // 
 
 #pragma once
-#include <iostream>
+#include <stdexcept>
 #include <cstdint>
 #include <istream>
 
@@ -15,19 +15,23 @@ class RecordBuffer
 	size_t bufidx, bufnumelements, recordsremaining;
 
 	void fillbuffer() {
+		if (recordsremaining == 0) {
+			throw std::range_error("no more data");
+		}
 		size_t numtoread = std::min(BUFFSIZE, recordsremaining);
 		infile.read((char*)buffer, sizeof(uint32_t) * numtoread);
+		if (!infile.good()) {
+			throw std::runtime_error("Error while reading TTTR records from infile. Unexpected end of file.");
+		}
 		recordsremaining -= numtoread;
 		bufnumelements = numtoread;
 		bufidx = 0;
-		if (!infile.good()) {
-			throw("Error while reading TTTR records from infile. Unexpected end of file.");
-		}
 	};
 public:
 	RecordBuffer(std::istream& InFile, size_t numrecords) : infile{ InFile }, buffer { new uint32_t[BUFFSIZE] },
 		bufidx { 0 }, bufnumelements{ 0 }, recordsremaining{ numrecords } {};
 	bool empty() { return bufidx == bufnumelements; };
+	bool noMoreData() { return empty() && recordsremaining == 0; };
 	// return and remove top element:
 	uint32_t pop() {
 		if (empty()) {
@@ -38,6 +42,9 @@ public:
 	// return but not remove top element:
 	uint32_t peek() {
 		if (empty()) {
+			if (recordsremaining == 0) {
+				throw std::range_error("tried to peek past last record");
+			}
 			fillbuffer();
 		}
 		return buffer[bufidx];
